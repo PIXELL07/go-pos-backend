700114e13f3f47a3b34874cfcabfcfc326f7c909
PIXELL07 <arunksn007@gmail.com>
2026-03-12 01:03:47 +0530

    config all internal n domain  roots

--- config/config.go
+++ config/config.go
@@ -1,1 +1,126 @@
 package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	App      AppConfig
+	Database DatabaseConfig
+	Redis    RedisConfig
+	JWT      JWTConfig
+	Google   GoogleConfig
+	CORS     CORSConfig
+}
+
+type AppConfig struct {
+	Env  string
+	Port string
+	Name string
+}
+
+type DatabaseConfig struct {
+	Host         string
+	Port         string
+	User         string
+	Password     string
+	Name         string
+	SSLMode      string
+	MaxOpenConns int
+	MaxIdleConns int
+}
+
+type RedisConfig struct {
+	Host     string
+	Port     string
+	Password string
+	DB       int
+}
+
+type JWTConfig struct {
+	Secret        string
+	AccessExpiry  time.Duration
+	RefreshExpiry time.Duration
+}
+
+type GoogleConfig struct {
+	ClientID     string
+	ClientSecret string
+	RedirectURL  string
+}
+
+type CORSConfig struct {
+	AllowedOrigins string
+}
+
+var cfg *Config
+
+func Load() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables")
+	}
+
+	accessExpiry, _ := time.ParseDuration(getEnv("JWT_ACCESS_EXPIRY", "15m"))
+	refreshExpiry, _ := time.ParseDuration(getEnv("JWT_REFRESH_EXPIRY", "168h"))
+	redisDB, _ := strconv.Atoi(getEnv("REDIS_DB", "0"))
+	maxOpenConns, _ := strconv.Atoi(getEnv("DB_MAX_OPEN_CONNS", "25"))
+	maxIdleConns, _ := strconv.Atoi(getEnv("DB_MAX_IDLE_CONNS", "5"))
+
+	cfg = &Config{
+		App: AppConfig{
+			Env:  getEnv("APP_ENV", "development"),
+			Port: getEnv("APP_PORT", "8080"),
+			Name: getEnv("APP_NAME", "POS Backend"),
+		},
+		Database: DatabaseConfig{
+			Host:         getEnv("DB_HOST", "localhost"),
+			Port:         getEnv("DB_PORT", "5432"),
+			User:         getEnv("DB_USER", "postgres"),
+			Password:     getEnv("DB_PASSWORD", ""),
+			Name:         getEnv("DB_NAME", "pos_db"),
+			SSLMode:      getEnv("DB_SSLMODE", "disable"),
+			MaxOpenConns: maxOpenConns,
+			MaxIdleConns: maxIdleConns,
+		},
+		Redis: RedisConfig{
+			Host:     getEnv("REDIS_HOST", "localhost"),
+			Port:     getEnv("REDIS_PORT", "6379"),
+			Password: getEnv("REDIS_PASSWORD", ""),
+			DB:       redisDB,
+		},
+		JWT: JWTConfig{
+			Secret:        getEnv("JWT_SECRET", "change-me-in-production"),
+			AccessExpiry:  accessExpiry,
+			RefreshExpiry: refreshExpiry,
+		},
+		Google: GoogleConfig{
+			ClientID:     getEnv("GOOGLE_CLIENT_ID", ""),
+			ClientSecret: getEnv("GOOGLE_CLIENT_SECRET", ""),
+			RedirectURL:  getEnv("GOOGLE_REDIRECT_URL", ""),
+		},
+		CORS: CORSConfig{
+			AllowedOrigins: getEnv("ALLOWED_ORIGINS", "*"),
+		},
+	}
+
+	return cfg
+}
+
+func Get() *Config {
+	if cfg == nil {
+		return Load()
+	}
+	return cfg
+}
+
+func getEnv(key, defaultVal string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return defaultVal
+}
