78dd1bbe3dc635f4228a796ecbb20b7ecd95cd51
PIXELL07 <arunksn007@gmail.com>
2026-03-15 21:10:25 +0530

    feat: commit

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,6 @@
 package logger
+
+// we r gonna use zap coz:
+// Uber's Zap is the preferred logging library in Go for high-performance applications, offering structured, leveled logging with exceptionally low overhead.
+// Developers use it because it avoids memory allocation and reflection, making it 4–10 times faster than other loggers.
+// It is ideal for microservices needing fast JSON logging, structured context, and log sampling
